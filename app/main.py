65968da2695b1cb75a6903edf24b8053bbbae17a
Amirammi <amirammina@gmail.com>
2024-04-09 13:59:10 +0300

    Added health_check func in main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
+from starlette import status
+
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+async def health_check() -> dict:
+    return {
+        "status_code": status.HTTP_200_OK,
+        "detail": "ok",
+        "result": "working"
+    }
