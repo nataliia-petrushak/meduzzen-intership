34aa85a104afb3c8313f616d3077d2c591b7d808
Nataliia Petrushak <87134904+nataliia-petrushak@users.noreply.github.com>
2024-04-10 11:04:48 +0300

    Update main.py, pass the application as an import string

--- app/main.py
+++ app/main.py
@@ -9,4 +9,4 @@ app.include_router(health.router)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host=settings.host, port=settings.port, reload=True)
+    uvicorn.run("main:app", host=settings.host, port=settings.port, reload=True)
