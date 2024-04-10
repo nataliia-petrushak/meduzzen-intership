57583fdc571b3a69afc97d8223ced142591ff597
Amirammi <amirammina@gmail.com>
2024-04-10 19:20:22 +0300

    Added CORSMiddleware and fixed config.py

--- app/config.py
+++ app/config.py
@@ -4,9 +4,14 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     host: str
     port: int
+    allowed_origins: list[str] = [
+        "http://localhost:8080",
+        "http://127.0.0.1:8000",
+        "http://0.0.0.0:8000"
+    ]
 
     class Config:
-        env_file = ".env"
+        env_file = [".env_docker", ".env"]
 
 
 settings = Settings()
