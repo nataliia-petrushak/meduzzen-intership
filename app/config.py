df179b306fabf9f2458be9eb655f01827fd439a4
Amirammi <amirammina@gmail.com>
2024-04-11 22:31:39 +0300

    Added new settings & fixed main.py

--- app/config.py
+++ app/config.py
@@ -1,9 +1,16 @@
 from pydantic_settings import BaseSettings
+from pydantic import PostgresDsn, validator
 
 
 class Settings(BaseSettings):
-    host: str
-    port: int
+    HOST: str
+    PORT: int
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+    DATABASE_HOST: str
+    DATABASE_PORT: str
+    DATABASE_NAME: str
+    REDIS_URL: str
     allowed_origins: list[str] = [
         "http://localhost:8080",
         "http://127.0.0.1:8000",
@@ -12,6 +19,11 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = ".env"
+
+    def get_db_url(self):
+        return (f"postgresql+asyncpg://{self.DATABASE_USER}:"
+                f"{self.DATABASE_PASSWORD}@{self.DATABASE_HOST}"
+                f":{self.DATABASE_PORT}/{self.DATABASE_NAME}")
 
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -15,4 +15,4 @@ app.include_router(health.router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=settings.host, port=settings.port, reload=True)
+    uvicorn.run("main:app", host=settings.HOST, port=settings.PORT, reload=True)
