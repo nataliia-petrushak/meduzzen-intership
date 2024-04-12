da6519734723114be0e7af5f0cdfc5896403de2a
Amirammi <amirammina@gmail.com>
2024-04-12 15:27:54 +0300

    Created User model in db/models.py

---
+++ app/db/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.database import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(
+        "id", autoincrement=True, unique=True, primary_key=True
+    )
+    username: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(50), nullable=False)
