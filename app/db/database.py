d36f6c5ad7700fb8e395268dbfce9fb9b967f95c
Amirammi <amirammina@gmail.com>
2024-04-14 22:20:20 +0300

    Fixed import in database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import settings
+from app.config import settings
 
 
 engine = create_async_engine(settings.postgres_url)
