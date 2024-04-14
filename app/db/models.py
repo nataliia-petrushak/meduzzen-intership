7d1a811d68bdec6b71e1bdbce5314e2296b9cdff
Amirammi <amirammina@gmail.com>
2024-04-14 18:34:51 +0300

    Added AbstractUser class with id field, fixed User class

--- app/db/models.py
+++ app/db/models.py
@@ -1,16 +1,17 @@
 from sqlalchemy import String, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
-from app.db.database import Base
+from db.database import Base
 
 
-class User(Base):
+class AbstractUser:
+    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
+
+
+class User(Base, AbstractUser):
     __tablename__ = "user"
 
-    id: Mapped[int] = mapped_column(
-        Integer, autoincrement=True, unique=True, primary_key=True, nullable=False
-    )
-    username: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(String(20), nullable=False)
     first_name: Mapped[str] = mapped_column(String(100), nullable=False)
     last_name: Mapped[str] = mapped_column(String(100), nullable=False)
     email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
