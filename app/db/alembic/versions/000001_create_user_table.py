a002bbfeb2faed51fa9283ff89eebd5301e657cf
Amirammi <amirammina@gmail.com>
2024-04-14 22:23:15 +0300

    Returned comment, fixed revision number & add UUID for id field

--- app/db/alembic/versions/000001_create_user_table.py
+++ app/db/alembic/versions/000001_create_user_table.py
@@ -2,9 +2,15 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy import UUID
 
+"""create user table
+Revision ID: 000001
+Revises: 
+Create Date: 2024-04-12 15:35:11.855991
+"""
 
-revision: str = "c5da83222d71"
+revision: str = "000001"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -13,7 +19,7 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     op.create_table(
         "user",
-        sa.Column("id", sa.Integer(), primary_key=True),
+        sa.Column("id", UUID(as_uuid=True), primary_key=True),
         sa.Column("username", sa.String(20), nullable=False),
         sa.Column("first_name", sa.String(100), nullable=False),
         sa.Column("last_name", sa.String(100), nullable=False),
