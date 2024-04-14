ac7c852b67f7c10a70dd1445703f94bc2a70cc64
Amirammi <amirammina@gmail.com>
2024-04-14 18:32:23 +0300

    Fixed alembic settings & migrations

--- app/db/alembic/versions/000001_create_user_table.py
+++ app/db/alembic/versions/000001_create_user_table.py
@@ -1,17 +1,9 @@
-"""create user table
-
-Revision ID: c5da83222d71
-Revises: 
-Create Date: 2024-04-12 15:35:11.855991
-
-"""
 from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
 
 
-# revision identifiers, used by Alembic.
 revision: str = "c5da83222d71"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
@@ -21,15 +13,8 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     op.create_table(
         "user",
-        sa.Column(
-            "id",
-            sa.Integer,
-            autoincrement=True,
-            unique=True,
-            primary_key=True,
-            nullable=False,
-        ),
-        sa.Column("username", sa.String(20), unique=True, nullable=False),
+        sa.Column("id", sa.Integer(), primary_key=True),
+        sa.Column("username", sa.String(20), nullable=False),
         sa.Column("first_name", sa.String(100), nullable=False),
         sa.Column("last_name", sa.String(100), nullable=False),
         sa.Column("email", sa.String(50), unique=True, nullable=False),
