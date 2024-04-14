6f3435b48d5866dac8b7e1e859623d77c6d55f16
Amirammi <amirammina@gmail.com>
2024-04-14 22:21:18 +0300

    Returned info, debug, warning, error, critical func in logger class

--- app/logger.py
+++ app/logger.py
@@ -9,5 +9,20 @@ class CustomLogger:
         logger.add(sys.stderr)
         self.logger = logger
 
+    def info(self, message):
+        self.logger.info(message)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
+
 
 custom_logger = CustomLogger()
