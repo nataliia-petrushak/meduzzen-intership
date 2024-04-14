eb9f354c09dd56d350d5fcc257f939512588c017
Amirammi <amirammina@gmail.com>
2024-04-14 18:34:17 +0300

    Fixed logger class

--- app/logger.py
+++ app/logger.py
@@ -1,25 +1,13 @@
+import sys
+
 from loguru import logger
 
 
 class CustomLogger:
-    def __init__(self, log_file: str = "logs/app.log"):
-        self.log_file = log_file
-        logger.add(self.log_file, rotation="500 MB", level="DEBUG")
-
-    def info(self, message):
-        logger.info(message)
-
-    def debug(self, message):
-        logger.debug(message)
-
-    def warning(self, message):
-        logger.warning(message)
-
-    def error(self, message):
-        logger.error(message)
-
-    def critical(self, message):
-        logger.critical(message)
+    def __init__(self):
+        logger.remove(0)
+        logger.add(sys.stderr)
+        self.logger = logger
 
 
 custom_logger = CustomLogger()
