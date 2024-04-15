5dee2a00abce0f664153ed8ea6fff377e90ac303
Amirammi <amirammina@gmail.com>
2024-04-15 22:57:22 +0300

    Added default id value for IDBase

--- app/db/alembic/versions/000001_create_user_table.py
+++ app/db/alembic/versions/000001_create_user_table.py
@@ -4,6 +4,7 @@ Revision ID: 000001
 Revises:
 Create Date: 2024-04-12 15:35:11.855991
 """
+import uuid
 from typing import Sequence, Union
 
 from alembic import op
@@ -20,7 +21,9 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     op.create_table(
         "user",
-        sa.Column("id", UUID(as_uuid=True), primary_key=True),
+        sa.Column(
+            "id", UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+        ),
         sa.Column("username", sa.String(20), nullable=False),
         sa.Column("email", sa.String(50), unique=True, nullable=False),
         sa.Column("password", sa.String(50), nullable=False),
--- app/db/models.py
+++ app/db/models.py
@@ -8,7 +8,9 @@ from app.db.database import Base
 
 class IDBase(Base):
     __abstract__ = True
-    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True)
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
 
 
 class UserBase(IDBase):
