afd4829c43602fbb8619b899caf71e661fa90292
Amirammi <amirammina@gmail.com>
2024-04-16 13:59:55 +0300

    Connected router for users

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from routers import health
+from routers import health, users
 from config import settings
 
 app = FastAPI()
@@ -10,6 +10,7 @@ app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=settings.allowed_origins)
 
 app.include_router(health.router)
+app.include_router(users.router)
 
 
 if __name__ == "__main__":
