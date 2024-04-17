649fc3cc3d855b421c5e83e8195b312c9b560edc
Amirammi <amirammina@gmail.com>
2024-04-17 09:24:43 +0300

    Added env variables & func to build test_database_url

--- app/config.py
+++ app/config.py
@@ -9,6 +9,11 @@ class Settings(BaseSettings):
     POSTGRES_HOST: str
     POSTGRES_PORT: str
     POSTGRES_NAME: str
+    TEST_DB_USER: str
+    TEST_DB_PASSWORD: str
+    TEST_DB_HOST: str
+    TEST_DB_PORT: str
+    TEST_DB_NAME: str
     REDIS_HOST: str
     REDIS_PORT: str
     REDIS_DB: str
@@ -28,6 +33,14 @@ class Settings(BaseSettings):
             f"postgresql+asyncpg://{self.POSTGRES_USER}"
             f":{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}"
             f":{self.POSTGRES_PORT}/{self.POSTGRES_NAME}"
+        )
+
+    @property
+    def test_postgres_url(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.TEST_DB_USER}"
+            f":{self.TEST_DB_PASSWORD}@{self.TEST_DB_HOST}"
+            f":{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
         )
 
     @property
