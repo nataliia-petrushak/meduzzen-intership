1951f6241d0262fdf6a88135d97752e0ccab040e
Amirammi <amirammina@gmail.com>
2024-04-18 21:14:04 +0300

    Created constants.py

---
+++ tests/constants.py
@@ -0,0 +1,12 @@
+user_signup_data = {
+    "username": "test_user",
+    "email": "test@example.com",
+    "password": "test_password",
+}
+
+
+user_update_data = {
+    "username": "Updated",
+    "email": "user@user.com",
+    "password": "<PASSWORD>",
+}
