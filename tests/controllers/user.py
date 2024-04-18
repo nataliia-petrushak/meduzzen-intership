6a2228ffb9ad9d37a4f9e3b5d92913d2bf75e937
Amirammi <amirammina@gmail.com>
2024-04-18 21:13:25 +0300

    Created dirs for test controllers & repos, moved tests

---
+++ tests/controllers/user.py
@@ -0,0 +1,57 @@
+import uuid
+
+from starlette import status
+from starlette.testclient import TestClient
+
+from tests.constants import user_signup_data, user_update_data
+
+
+def test_create_user_endpoint(client: TestClient) -> None:
+    response = client.post("/users/", json=user_signup_data)
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.json()["username"] == user_signup_data["username"]
+
+
+def test_can_not_create_user_with_bad_data(client: TestClient) -> None:
+    response = client.post("/users/", json=user_signup_data.update({"name": "Ilona"}))
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+async def test_get_user_list_endpoint(client: TestClient) -> None:
+    response = client.get("/users/")
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_get_user_by_id_endpoint(client: TestClient) -> None:
+    users = client.get("/users/")
+    user_id = users.json()[0]["id"]
+    response = client.get(f"/users/{user_id}/")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["id"] == user_id
+
+    key_2 = uuid.uuid4()
+    response_2 = client.get(f"/users/{key_2}/")
+    assert response_2.status_code == status.HTTP_404_NOT_FOUND
+
+
+async def test_update_user_endpoint(client: TestClient) -> None:
+    users = client.get("/users/")
+    user_id = users.json()[0]["id"]
+    response = client.patch(f"/users/{user_id}/", json=user_update_data)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["username"] == user_update_data["username"]
+    assert response.json()["email"] == user_update_data["email"]
+
+
+def test_delete_user_endpoint(client: TestClient) -> None:
+    users = client.get("/users/")
+    user_id = users.json()[0]["id"]
+    response = client.delete(f"/users/{user_id}/")
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    response_2 = client.get(f"/users/{user_id}/")
+
+    assert response_2.status_code == status.HTTP_404_NOT_FOUND
---
+++ tests/repos/user.py
@@ -0,0 +1,98 @@
+import pytest
+
+from db.alembic.repos.user_repo import UserRepository
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from schemas.users import UserSignUp, UserUpdate
+from tests.conftest import AsyncSessionLocal
+
+
+@pytest.fixture
+def user_repo() -> UserRepository:
+    return UserRepository()
+
+
+@pytest.fixture
+async def db() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        yield session
+
+
+@pytest.fixture
+def pydentic_create_data():
+    return UserSignUp(username="Afanasiy", email="email@email.com", password="1234567")
+
+
+@pytest.fixture
+def pydentic_update_data():
+    return UserUpdate(username="Bruno", email="email@bruno.com", password="<PASSWORD>")
+
+
+@pytest.mark.asyncio(scope="function")
+async def test_add_user_func(
+    pydentic_create_data: UserSignUp, user_repo: UserRepository, db: AsyncSession
+) -> None:
+    result = await user_repo.create_model(db=db, model_data=pydentic_create_data)
+
+    assert result["username"] == pydentic_create_data.username
+    assert result["email"] == pydentic_create_data.email
+
+
+@pytest.mark.asyncio(scope="function")
+async def test_get_user_list_func(db: AsyncSession, user_repo: UserRepository):
+    await user_repo.create_model(
+        db=db,
+        model_data=UserSignUp(username="Test", email="user1@user.com", password="123"),
+    )
+    await user_repo.create_model(
+        db=db,
+        model_data=UserSignUp(username="Test1", email="user2@user.com", password="123"),
+    )
+    await user_repo.create_model(
+        db=db,
+        model_data=UserSignUp(username="Test2", email="user3@user.com", password="123"),
+    )
+
+    result = await user_repo.get_model_list(db=db, limit=10, offset=0)
+
+    assert len(result) == 4
+    assert result[1].username == "Test"
+    assert result[2].username == "Test1"
+    assert result[3].username == "Test2"
+
+
+@pytest.mark.asyncio(scope="function")
+async def test_get_user_by_id_func(db: AsyncSession, user_repo: UserRepository):
+    user = await user_repo.get_model_list(db=db, limit=1, offset=0)
+    user_id = user[0].id
+    result = await user_repo.get_model_by_id(db=db, model_id=user_id)
+
+    assert result.id == user_id
+    assert result.username == user[0].username
+    assert result.email == user[0].email
+
+
+@pytest.mark.asyncio(scope="function")
+async def test_update_user(
+    pydentic_update_data: UserUpdate, db: AsyncSession, user_repo: UserRepository
+):
+    user = await user_repo.get_model_list(db=db, limit=1, offset=0)
+    user_id = user[0].id
+    result = await user_repo.update_model(
+        db=db, model_id=user_id, model_data=pydentic_update_data
+    )
+
+    assert result.id == user_id
+    assert result.username == pydentic_update_data.username
+    assert result.email == pydentic_update_data.email
+
+
+@pytest.mark.asyncio(scope="function")
+async def test_delete_user(db: AsyncSession, user_repo: UserRepository):
+    user = await user_repo.get_model_list(db=db, limit=1, offset=0)
+    user_id = user[0].id
+    await user_repo.delete_model(db=db, model_id=user_id)
+
+    result = await user_repo.get_model_list(db=db, limit=10, offset=0)
+
+    assert user not in result
