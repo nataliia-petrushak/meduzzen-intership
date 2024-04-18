a2e35d3f8bb3753dbc361400c57c633e4699d9e9
Amirammi <amirammina@gmail.com>
2024-04-18 21:14:31 +0300

    Fixed security logic

--- app/services/security.py
+++ app/services/security.py
@@ -1,12 +1,10 @@
 from passlib.context import CryptContext
 
 
-class PasswordHelper:
-    def __init__(self):
-        self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-    def hash_password(self, password: str) -> str:
-        return self.pwd_context.hash(password)
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-password_helper = PasswordHelper()
+class SecurityService:
+    @staticmethod
+    def hash_password(password: str) -> str:
+        return pwd_context.hash(password)
