966656825d3bf574d13f03300013ab3c81539daf
Amirammi <amirammina@gmail.com>
2024-04-18 21:13:54 +0300

    Changed async client to sync & fixed test settings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,12 @@
 from typing import AsyncGenerator
 
-import httpx
 import pytest
-from httpx import AsyncClient
 from sqlalchemy import NullPool
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_session
 from sqlalchemy.orm import sessionmaker
-from app.db.models import Base
+from starlette.testclient import TestClient
+
+from db.models import Base
 
 from app.main import app
 from dependencies import get_db
@@ -20,13 +20,12 @@ AsyncSessionLocal = sessionmaker(
 )
 
 
-@pytest.fixture(scope="function", autouse=True)
-async def session_override():
-    async def test_db() -> AsyncSession:
-        async with AsyncSessionLocal() as db:
-            yield db
+async def override_get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as db:
+        yield db
 
-    app.dependency_overrides[get_db] = test_db
+
+app.dependency_overrides[get_db] = override_get_db
 
 
 @pytest.fixture(autouse=True, scope="session")
@@ -38,9 +37,7 @@ async def prepare_db():
         await conn.run_sync(Base.metadata.drop_all)
 
 
-@pytest.fixture(scope="session")
-async def ac() -> AsyncGenerator[AsyncClient, None]:
-    async with AsyncClient(
-        transport=httpx.ASGITransport(app=app), base_url="http://test"
-    ) as ac:
-        yield ac
+@pytest.fixture(name="client", scope="session")
+def client():
+    client = TestClient(app)
+    yield client
