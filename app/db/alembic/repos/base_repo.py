cb4ab2adfe56c49a7186bcf02b0826fcafac126b
Amirammi <amirammina@gmail.com>
2024-04-19 15:50:47 +0300

    Added Error handling

--- app/db/alembic/repos/base_repo.py
+++ app/db/alembic/repos/base_repo.py
@@ -1,11 +1,10 @@
 from uuid import UUID
 
-from fastapi import HTTPException
 from sqlalchemy import select, insert, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from db.models import Base
-from starlette import status
+from app.db.models import Base
+from app.utils.common import UserNotFound
 
 
 class BaseRepository:
@@ -19,36 +18,45 @@ class BaseRepository:
         models = await db.execute(query)
         return [model[0] for model in models.fetchall()]
 
-    async def get_model_by_id(self, db: AsyncSession, model_id: UUID) -> Base | None:
-        result = await db.execute(select(self.model).filter(self.model.id == model_id))
+    async def get_model_by_id(self, db: AsyncSession, model_id: UUID) -> Base:
+        result = await db.execute(
+            select(self.model, self.model.id).filter(self.model.id == model_id)
+        )
         model = result.scalar()
 
         if not model:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, detail="User is not found"
-            )
+            raise UserNotFound(object_id=model_id)
 
         return model
 
     async def create_model(self, db: AsyncSession, model_data: dict) -> Base:
-        query = insert(self.model).values(**model_data)
+        query = insert(self.model).values(**model_data).returning(self.model)
         result = await db.execute(query)
         await db.commit()
-        return {**model_data, "id": result.inserted_primary_key}
+        return result.scalars().one_or_none()
 
     async def update_model(
         self, db: AsyncSession, model_id: UUID, model_data: dict
     ) -> Base:
-        model = await self.get_model_by_id(db=db, model_id=model_id)
-        query = (
-            update(self.model).filter(self.model.id == model_id).values(**model_data)
+        result = await db.execute(
+            update(self.model)
+            .where(self.model.id == model_id)
+            .values(**model_data)
+            .returning(self.model)
         )
-
-        await db.execute(query)
         await db.commit()
+        model = result.scalar()
+
+        if not model:
+            raise UserNotFound(object_id=model_id)
         return model
 
-    async def delete_model(self, db: AsyncSession, model_id: UUID) -> None:
-        await self.get_model_by_id(db=db, model_id=model_id)
-        await db.execute(delete(self.model).filter(self.model.id == model_id))
+    async def delete_model(self, db: AsyncSession, model_id: UUID) -> Base:
+        result = await db.execute(
+            delete(self.model).where(self.model.id == model_id).returning(self.model)
+        )
         await db.commit()
+        model = result.scalar()
+        if not model:
+            raise UserNotFound(object_id=model_id)
+        return model
