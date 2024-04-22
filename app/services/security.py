1ffd57586bdf39a98a0f94941bd7fcd03d3be4c9
Amirammi <amirammina@gmail.com>
2024-04-22 21:25:18 +0300

    Added functionality to encode & decode token in security.py

--- app/services/security.py
+++ app/services/security.py
@@ -1,5 +1,8 @@
-from passlib.context import CryptContext
+from datetime import datetime, timedelta
 
+import jwt
+from passlib.context import CryptContext
+from app.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -8,3 +11,24 @@ class SecurityService:
     @staticmethod
     def hash_password(password: str) -> str:
         return pwd_context.hash(password)
+
+    @staticmethod
+    def verify_password(password: str, hashed_password: str) -> bool:
+        return pwd_context.verify(password, hashed_password)
+
+    @staticmethod
+    def encode_user_token(email: str) -> str:
+        payload = {
+            "email": email,
+            "expires": f"{datetime.utcnow() + timedelta(minutes=10)}",
+        }
+        token = jwt.encode(
+            payload, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHMS
+        )
+        return token
+
+    @staticmethod
+    def decode_user_token(token: str) -> dict:
+        return jwt.decode(
+            token, settings.JWT_SECRET, algorithms=[settings.JWT_ALGORITHMS]
+        )
