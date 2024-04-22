d3b428dec5864000599622aa830cf327b282da8c
Amirammi <amirammina@gmail.com>
2024-04-22 21:24:11 +0300

    Registered Authorization Error in main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,8 @@ from starlette.responses import JSONResponse
 
 from app.routers import health, user
 from app.config import settings
-from app.core.exceptions import ObjectNotFound
+from app.core.exceptions import ObjectNotFound, AuthorizationError
+
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=settings.allowed_origins)
@@ -18,6 +19,13 @@ app.include_router(user.router)
 async def user_not_found_handler(request: Request, exc: ObjectNotFound):
     return JSONResponse(
         status_code=status.HTTP_404_NOT_FOUND, content={"message": exc.msg}
+    )
+
+
+@app.exception_handler(AuthorizationError)
+async def authorization_error_handler(request: Request, exc: AuthorizationError):
+    return JSONResponse(
+        status_code=status.HTTP_403_FORBIDDEN, content={"message": exc.msg}
     )
 
 
