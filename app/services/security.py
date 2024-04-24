ea9b4224fcdf6b66fa3dab2258973bc477df85b3
Amirammi <amirammina@gmail.com>
2024-04-24 18:35:55 +0300

    Moved Auth0Security to security.py

--- app/services/security.py
+++ app/services/security.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 import jwt
 from passlib.context import CryptContext
 from app.config import settings
+from app.core.exceptions import AuthorizationError
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -32,3 +33,25 @@ class SecurityService:
         return jwt.decode(
             token, settings.JWT_SECRET, algorithms=[settings.JWT_ALGORITHMS]
         )
+
+
+class Auth0Security:
+    @staticmethod
+    def verify(token: str):
+        jwt_client = jwt.PyJWKClient(settings.auth_url)
+
+        try:
+            signing_key = jwt_client.get_signing_key_from_jwt(token).key
+        except (jwt.exceptions.PyJWKClientError, jwt.exceptions.DecodeError) as error:
+            raise AuthorizationError(detail=str(error))
+        try:
+            payload = jwt.decode(
+                token,
+                key=signing_key,
+                algorithms=[settings.AUTH0_ALGORITHMS],
+                audience=settings.AUTH0_IDENTIFIER,
+                issuer=settings.AUTH0_ISSUER,
+            )
+        except jwt.PyJWTError as error:
+            raise AuthorizationError(detail=str(error))
+        return payload
