55ab4f9308cb49379b0414093107e4cdef4229c2
Amirammi <amirammina@gmail.com>
2024-04-24 11:30:22 +0300

    Moved logic of AuthBearer to another file

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,42 +1,17 @@
-import jwt
-from fastapi import Security, Depends
-from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi import Depends, Security
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.exceptions import AuthorizationError, UserNotFound
+from app.core.exceptions import UserNotFound
 from app.dependencies import get_db
 from app.schemas.users import GetUser, UserSignUp
-from app.services.auth0 import Auth0Service
-from app.services.security import SecurityService
 from app.services.user import UserService
-
-security = HTTPBearer()
-user_service = UserService()
-
-
-def jwt_authenticate(token: str) -> dict | None:
-    try:
-        return SecurityService.decode_user_token(token)
-    except jwt.exceptions.InvalidAlgorithmError:
-        return None
-
-
-def auth0_authenticate(token: str) -> dict | None:
-    try:
-        return Auth0Service.verify(token)
-    except AuthorizationError:
-        return None
-
-
-def authorize_user(token: HTTPAuthorizationCredentials = Security(security)):
-    payload_1 = auth0_authenticate(token.credentials)
-    payload_2 = jwt_authenticate(token.credentials)
-    return payload_1 or payload_2
+from app.utils.auth_bearer import auth_bearer
 
 
 async def get_authenticated_user(
-        db: AsyncSession = Depends(get_db),
-        payload: dict = Depends(authorize_user)
+    db: AsyncSession = Depends(get_db),
+    user_service: UserService = Depends(UserService),
+    payload: dict = Security(auth_bearer),
 ) -> GetUser:
     user_email = payload["email"]
     username = user_email.split("@")[0]
