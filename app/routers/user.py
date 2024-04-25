7b39389cb7071c616a7c2cdfdb04b0774931b0e1
Amirammi <amirammina@gmail.com>
2024-04-25 21:49:51 +0300

    Check permissions inside services

--- app/routers/user.py
+++ app/routers/user.py
@@ -4,9 +4,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
 from app.db.database import get_db
-from app.schemas.users import GetUser, UserDetail, UserUpdate
-from app.services.user import UserService
 from app.dependencies import check_permissions
+from app.schemas.users import GetUser, UserDetail, UserUpdate
+from app.services.auth import get_authenticated_user
+from app.services.user import UserService
 
 
 router = APIRouter(tags=["users"], prefix="/users")
@@ -35,10 +36,11 @@ async def get_user_by_id(
 async def update_user(
     user_id: UUID,
     user_data: UserUpdate,
-    user: GetUser = Depends(check_permissions),
+    user: GetUser = Depends(get_authenticated_user),
     db: AsyncSession = Depends(get_db),
     user_service: UserService = Depends(UserService),
 ) -> UserUpdate:
+    await check_permissions(user_id=user_id, user=user)
     return await user_service.update_model(
             model_data=user_data, db=db, model_id=user_id
         )
@@ -47,8 +49,9 @@ async def update_user(
 @router.patch("/{user_id}/deactivate", status_code=status.HTTP_204_NO_CONTENT)
 async def deactivate_user(
     user_id: UUID,
-    user: GetUser = Depends(check_permissions),
+    user: GetUser = Depends(get_authenticated_user),
     db: AsyncSession = Depends(get_db),
     user_service: UserService = Depends(UserService),
 ) -> None:
+    await check_permissions(user_id=user_id, user=user)
     return await user_service.user_deactivate(user_id=user_id, db=db)
