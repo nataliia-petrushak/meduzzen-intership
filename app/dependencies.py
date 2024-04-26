14fa29e38dfbbfb1c3ec54b84f6744066d7729d7
Amirammi <amirammina@gmail.com>
2024-04-26 14:52:03 +0300

    Added permissions for checking company owner

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,9 +1,26 @@
 from uuid import UUID
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
 from app.core.exceptions import AccessDeniedError
+from app.db.models import User, Company
 from app.schemas.users import GetUser
 
 
-async def check_permissions(user_id: UUID, user: GetUser) -> None:
+def check_permissions(user_id: UUID, user: GetUser) -> None:
     if user_id != user.id:
+        raise AccessDeniedError()
+
+
+async def check_company_owner(
+    db: AsyncSession, company_id: UUID, user: GetUser
+) -> None:
+    result = await db.execute(
+        select(Company.id)
+        .join(User, Company.owner_id == User.id)
+        .filter(Company.owner_id == user.id)
+    )
+    company = [company[0] for company in result.fetchall() if company[0] == company_id]
+    if not company:
         raise AccessDeniedError()
