3e83ac2acb1f70dd9cf7c988e21309f540e80013
Amirammi <amirammina@gmail.com>
2024-04-28 15:34:03 +0300

    Deleted check_company_owner

--- app/dependencies.py
+++ app/dependencies.py
@@ -11,16 +11,3 @@ from app.schemas.users import GetUser
 def check_permissions(user_id: UUID, user: GetUser) -> None:
     if user_id != user.id:
         raise AccessDeniedError()
-
-
-async def check_company_owner(
-    db: AsyncSession, company_id: UUID, user: GetUser
-) -> None:
-    result = await db.execute(
-        select(Company.id)
-        .join(User, Company.owner_id == User.id)
-        .filter(Company.owner_id == user.id)
-    )
-    company = [company[0] for company in result.fetchall() if company[0] == company_id]
-    if not company:
-        raise AccessDeniedError()
