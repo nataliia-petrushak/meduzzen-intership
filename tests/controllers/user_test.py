1f1cee47d9e4dda380eb036af923afccea6207dd
Amirammi <amirammina@gmail.com>
2024-04-29 22:01:09 +0300

    Changed user.py to user_test.py

---
+++ tests/controllers/user_test.py
@@ -0,0 +1,48 @@
+from uuid import UUID
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from tests.constants import user_signup_data, user_update_data, user_bad_data, user_payload
+
+
+async def test_get_user_list_endpoint(
+    client: TestClient, prepare_database, fill_database
+) -> None:
+    response = client.get("users/")
+    result = response.json()
+    assert response.status_code == status.HTTP_200_OK
+    assert result[0]["username"] == user_payload[0]["username"]
+    assert result[1]["username"] == user_payload[1]["username"]
+    assert result[2]["username"] == user_payload[2]["username"]
+
+
+def test_get_user_by_id_endpoint(
+    client: TestClient, user_id: UUID, prepare_database, fill_database
+) -> None:
+    response = client.get(f"users/{user_id}")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["id"] == str(user_id)
+
+    response_2 = client.get(f"/users/af3efcf6-9c61-4865-832f-5250f7fb8aec")
+    assert response_2.status_code == status.HTTP_404_NOT_FOUND
+
+
+async def test_update_user_endpoint(
+    client: TestClient, user_id: UUID, prepare_database, fill_database, token
+) -> None:
+    response = client.patch(
+        f"users/{user_id}", json=user_update_data, headers={"Authorization": f"Bearer {token}"}
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["username"] == user_update_data["username"]
+
+
+def test_delete_user_endpoint(
+    client: TestClient, user_id: UUID, prepare_database, fill_database, token
+) -> None:
+    response = client.patch(
+        f"users/{user_id}/deactivate", headers={"Authorization": f"Bearer {token}"}
+    )
+    assert response.status_code == status.HTTP_204_NO_CONTENT
--- tests/repos/user_test.py
+++ tests/repos/user_test.py
@@ -1,17 +1,15 @@
 from uuid import UUID
 
 import pytest
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.db.models import User
-from app.core.exceptions import UserNotFound
+from app.core.exceptions import ObjectNotFound
 from tests.constants import pydentic_update_data, pydentic_create_data, user_repo
 
 
 @pytest.mark.asyncio
 async def test_add_user_func(db: AsyncSession) -> None:
-    result = await user_repo.create_model(db=db, model_data=pydentic_create_data)
+    result = await user_repo.create_model(db=db, model_data=pydentic_create_data.model_dump())
 
     assert result.username == pydentic_create_data.username
     assert result.email == pydentic_create_data.email
@@ -21,22 +19,22 @@ async def test_add_user_func(db: AsyncSession) -> None:
 async def test_get_user_list_func(db: AsyncSession, fill_database) -> None:
     result = await user_repo.get_model_list(db=db, limit=10, offset=0)
 
-    assert len(result) == 3
-    assert result[0].username == "test_1"
-    assert result[1].username == "test_2"
-    assert result[2].username == "test_3"
+    assert len(result) == 5
+    assert result[1].username == "test_1"
+    assert result[2].username == "test_2"
+    assert result[3].username == "test_3"
 
 
 @pytest.mark.asyncio
 async def test_get_user_by_id_func(
     db: AsyncSession, user_id: UUID, fill_database
 ) -> None:
-    with pytest.raises(UserNotFound):
-        await user_repo.get_model_by_id(
-            db=db, model_id="af3efcf6-9c61-4865-832f-5250f7fb8aec"
+    with pytest.raises(ObjectNotFound):
+        await user_repo.get_model_by(
+            db=db, filters={"id": "af3efcf6-9c61-4865-832f-5250f7fb8aec"}
         )
 
-    result = await user_repo.get_model_by_id(db=db, model_id=user_id)
+    result = await user_repo.get_model_by(db=db, filters={"id": user_id})
 
     assert result.id == user_id
     assert result.username == "test_1"
@@ -45,25 +43,24 @@ async def test_get_user_by_id_func(
 
 @pytest.mark.asyncio
 async def test_update_user(db: AsyncSession, user_id: UUID, fill_database) -> None:
-    with pytest.raises(UserNotFound):
+    with pytest.raises(ObjectNotFound):
         await user_repo.update_model(
             db=db,
             model_id="af3efcf6-9c61-4865-832f-5250f7fb8aec",
-            model_data=pydentic_update_data,
+            model_data=pydentic_update_data.model_dump(),
         )
 
     result = await user_repo.update_model(
-        db=db, model_id=user_id, model_data=pydentic_update_data
+        db=db, model_id=user_id, model_data=pydentic_update_data.model_dump()
     )
 
     assert result.id == user_id
     assert result.username == pydentic_update_data.username
-    assert result.email == pydentic_update_data.email
 
 
 @pytest.mark.asyncio
 async def test_delete_user(db: AsyncSession, user_id: UUID, fill_database) -> None:
-    with pytest.raises(UserNotFound):
+    with pytest.raises(ObjectNotFound):
         await user_repo.delete_model(
             db=db, model_id="af3efcf6-9c61-4865-832f-5250f7fb8aec"
         )
