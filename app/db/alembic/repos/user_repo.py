9839de321e1a25f8d76498be2a4017c6bdee6568
Amirammi <amirammina@gmail.com>
2024-04-29 22:04:35 +0300

    Fixed deleted unnecessary methods in user & company repos

--- app/db/alembic/repos/user_repo.py
+++ app/db/alembic/repos/user_repo.py
@@ -9,11 +9,3 @@ from app.db.alembic.repos.base_repo import BaseRepository
 class UserRepository(BaseRepository):
     def __init__(self):
         super().__init__(User)
-
-    async def get_user_by_email(self, db: AsyncSession, email: str) -> User:
-        result = await db.execute(select(self.model).filter(self.model.email == email))
-        user = result.scalar()
-
-        if not user:
-            raise ObjectNotFound(identifier=email, model_name=self.model.__name__)
-        return user
