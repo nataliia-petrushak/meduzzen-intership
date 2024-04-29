518fb1435b8e768e0dab823f5319efd4ffc7b503
Amirammi <amirammina@gmail.com>
2024-04-29 22:05:03 +0300

    Wrote tests for company

---
+++ tests/controllers/company_test.py
@@ -0,0 +1,112 @@
+from uuid import UUID
+
+import pytest
+from starlette import status
+from starlette.testclient import TestClient
+
+from tests.constants import company_data, company_payload, company_update_data
+
+
+@pytest.mark.asyncio
+async def test_create_company(client: TestClient, owner_token: str, prepare_database) -> None:
+    response = client.post(
+        "/companies", json=company_data, headers={"Authorization": f"Bearer {owner_token}"}
+    )
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.json()["name"] == company_data["name"]
+    assert response.json()["description"] == company_data["description"]
+    assert response.json()["is_hidden"] == company_data["is_hidden"]
+
+
+@pytest.mark.asyncio
+async def test_can_not_create_company_unauthorized(client: TestClient, prepare_database) -> None:
+    response = client.post("/companies", json=company_data)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.asyncio
+async def test_get_company_list(
+        client: TestClient, prepare_database, fill_database_with_companies
+) -> None:
+    response = client.get("/companies")
+    result = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert result[0]["name"] == company_payload[0]["name"]
+    assert result[1]["name"] == company_payload[1]["name"]
+    assert result[2]["name"] == company_payload[2]["name"]
+
+
+@pytest.mark.asyncio
+async def test_get_company_by_id(
+        client: TestClient, company_id: UUID, prepare_database, fill_database_with_companies
+) -> None:
+    response = client.get(f"/companies/{company_id}")
+    result = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert result["name"] == company_payload[0]["name"]
+
+    response_2 = client.get(f"/companies/af3efcf6-9c61-4865-832f-5250f7fb8aec")
+    assert response_2.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.asyncio
+async def test_update_company_unauthorized(
+        client: TestClient,
+        company_id: UUID,
+        prepare_database,
+        fill_database_with_companies,
+) -> None:
+    response = client.patch(f"/companies/{company_id}", json=company_update_data)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.asyncio
+async def test_update_company_authorized_forbidden(
+        client: TestClient,
+        company_id: UUID,
+        owner_token: str,
+        prepare_database,
+        fill_database_with_companies,
+) -> None:
+    response = client.patch(
+        f"/companies/{company_id}",
+        json=company_update_data,
+        headers={"Authorization": f"Bearer {owner_token}"}
+    )
+    result = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert result["name"] == company_update_data["name"]
+    assert result["is_hidden"] == company_update_data["is_hidden"]
+
+
+@pytest.mark.asyncio
+async def test_delete_company_unauthorized_forbidden(
+        client: TestClient,
+        company_id: UUID,
+        prepare_database,
+        fill_database_with_companies,
+) -> None:
+    response = client.delete(f"/companies/{company_id}")
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.asyncio
+async def test_delete_company_unauthorized(
+        client: TestClient,
+        company_id: UUID,
+        owner_token: str,
+        prepare_database,
+        fill_database_with_companies,
+) -> None:
+    response = client.delete(
+        f"/companies/{company_id}", headers={"Authorization": f"Bearer {owner_token}"}
+    )
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
---
+++ tests/repos/company_test.py
@@ -0,0 +1,71 @@
+from uuid import UUID
+
+import pytest
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.exceptions import ObjectNotFound
+from tests.constants import company_repo, company_data, company_update_data
+
+
+@pytest.mark.asyncio
+async def test_add_company_func(db: AsyncSession, user_id: UUID) -> None:
+    company_data["owner_id"] = user_id
+    result = await company_repo.create_model(db=db, model_data=company_data)
+
+    assert result.name == company_data["name"]
+    assert result.is_hidden == company_data["is_hidden"]
+
+
+@pytest.mark.asyncio
+async def test_get_company_list_func(db: AsyncSession, fill_database_with_companies) -> None:
+    result = await company_repo.get_model_list(db=db, limit=10, offset=0)
+
+    assert len(result) == 3
+    assert result[0].name == "test_1"
+    assert result[1].name == "test_2"
+    assert result[2].name == "test_3"
+
+
+@pytest.mark.asyncio
+async def test_get_company_by_id_func(
+    db: AsyncSession, company_id: UUID, fill_database_with_companies
+) -> None:
+    with pytest.raises(ObjectNotFound):
+        await company_repo.get_model_by(
+            db=db, filters={"id": "af3efcf6-9c61-4865-832f-5250f7fb8aec"}
+        )
+
+    result = await company_repo.get_model_by(db=db, filters={"id": company_id})
+
+    assert result.id == company_id
+    assert result.name == "test_1"
+
+
+@pytest.mark.asyncio
+async def test_update_company(
+        db: AsyncSession, company_id: UUID, fill_database_with_companies
+) -> None:
+    with pytest.raises(ObjectNotFound):
+        await company_repo.update_model(
+            db=db,
+            model_id="af3efcf6-9c61-4865-832f-5250f7fb8aec",
+            model_data=company_update_data,
+        )
+
+    result = await company_repo.update_model(
+        db=db, model_id=company_id, model_data=company_update_data
+    )
+
+    assert result.id == company_id
+    assert result.name == company_update_data["name"]
+
+
+@pytest.mark.asyncio
+async def test_delete_company(db: AsyncSession, company_id: UUID, fill_database_with_companies) -> None:
+    with pytest.raises(ObjectNotFound):
+        await company_repo.delete_model(
+            db=db, model_id="af3efcf6-9c61-4865-832f-5250f7fb8aec"
+        )
+
+    result = await company_repo.delete_model(db=db, model_id=company_id)
+    assert company_id == result.id
