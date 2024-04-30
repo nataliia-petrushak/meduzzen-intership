b379b1244744abe41cb2290b8a3a431f8d5015b3
Amirammi <amirammina@gmail.com>
2024-04-30 13:14:49 +0300

    Added uniqueconstraint, fixed enum field, added enum drop

--- app/db/alembic/versions/000004_create_invitation_table_added_.py
+++ app/db/alembic/versions/000004_create_invitation_table_added_.py
@@ -12,8 +12,6 @@ from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects.postgresql import ENUM
 
-from app.db.models import Status
-
 # revision identifiers, used by Alembic.
 revision: str = "000004"
 down_revision: Union[str, None] = "000003"
@@ -21,11 +19,17 @@ branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
+def _get_request_type() -> ENUM:
+    return ENUM("invitation", "join_request", "member", name="request_type")
+
+
 def upgrade() -> None:
+    request_type = _get_request_type()
+    request_type.create(op.get_bind())
     op.create_table(
         "request",
         sa.Column("id", UUID(as_uuid=True), primary_key=True, default=uuid.uuid4),
-        sa.Column("status", ENUM("INVITATION", "JOIN_REQUEST", "MEMBER", name="status"), nullable=False),
+        sa.Column("request_type", request_type, nullable=False),
         sa.Column(
             "company_id",
             UUID(as_uuid=True),
@@ -33,12 +37,17 @@ def upgrade() -> None:
             nullable=False,
         ),
         sa.Column(
-            "user_id", UUID(as_uuid=True), sa.ForeignKey("user.id", ondelete="CASCADE"), nullable=False
+            "user_id",
+            UUID(as_uuid=True),
+            sa.ForeignKey("user.id", ondelete="CASCADE"),
+            nullable=False,
         ),
         sa.ForeignKeyConstraint(["company_id"], ["company.id"]),
         sa.ForeignKeyConstraint(["user_id"], ["user.id"]),
+        sa.UniqueConstraint("company_id", "user_id", name="_user_company_uc"),
     )
 
 
 def downgrade() -> None:
     op.drop_table("invitation")
+    _get_request_type().drop(op.get_bind())
