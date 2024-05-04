6caf2a8ba2e6dc4782ccd226ad2cc15cdcb35381
Amirammi <amirammina@gmail.com>
2024-05-04 13:42:18 +0300

    Delete validation for answers amount & add None nype to rating field

--- app/schemas/quiz_result.py
+++ app/schemas/quiz_result.py
@@ -7,12 +7,8 @@ from pydantic import BaseModel, Field
 class Answers(BaseModel):
     question: str
     answers: list
+    is_correct: bool
 
-    @field_validator("answers")
-    def validate_answers(cls, answers: list) -> list:
-        if len(answers) == 0:
-            raise ValidationError(detail="Question must have at least one answer")
-        return answers
 
 class AllQuizResults(BaseModel):
     date: datetime = Field(default_factory=datetime.now)
@@ -32,4 +28,4 @@ class GetQuizResult(QuizResultBase):
 
 
 class Rating(BaseModel):
-    rating: float
+    rating: float | None
