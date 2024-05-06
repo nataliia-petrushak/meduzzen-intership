965aa8137d527737a0d5ed34fac6ac5ebd27c73b
Amirammi <amirammina@gmail.com>
2024-05-06 22:31:54 +0300

    Added pickle to redis

--- app/db/redis.py
+++ app/db/redis.py
@@ -1,3 +1,4 @@
+import pickle
 from datetime import timedelta
 from typing import Any
 
@@ -8,15 +9,15 @@ from app.config import settings
 class DBRedisManager:
     def __init__(self):
         self._pool = ConnectionPool.from_url(
-            settings.redis_url, max_connections=10, decode_responses=True
+            settings.redis_url, max_connections=10, decode_responses=False
         )
         self._redis_client = Redis(connection_pool=self._pool)
 
     async def get_value(self, key: Any) -> dict:
         if value := await self._redis_client.get(key):
-            return {"key": key, "value": value.decode()}
+            return {"key": key, "value": pickle.loads(value)}
         return {"message": "Key not found in Redis"}
 
     async def set_value(self, key: Any, value: Any) -> dict:
-        await self._redis_client.set(key, value, ex=timedelta(hours=48))
+        await self._redis_client.set(key, pickle.dumps(value), ex=timedelta(hours=48))
         return {"message": f"Value '{value}' written to Redis with key '{key}'"}
