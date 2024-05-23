e8085afc0e00ee879996a32aa39c6cdca3e28be8
Amirammi <amirammina@gmail.com>
2024-05-23 22:49:17 +0300

    Added validations for user, request & quiz pydentic schemas

--- app/schemas/request.py
+++ app/schemas/request.py
@@ -1,26 +1,22 @@
 from uuid import UUID
 from pydantic import BaseModel
 
-from app.db.models import RequestType
 from app.schemas.company import GetCompany
 from app.schemas.users import GetUser
 
 
 class RequestBase(BaseModel):
-    request_type: RequestType
+    id: UUID
 
 
 class GetRequest(RequestBase):
-    id: UUID
     company_id: UUID
     user_id: UUID
 
 
 class CompanyRequest(RequestBase):
-    id: UUID
     company: GetCompany
 
 
 class UserRequest(RequestBase):
-    id: UUID
     user: GetUser
